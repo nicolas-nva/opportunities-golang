6f8e49a280f87518c4fa55205f280c7d63709e17
drxf <nicolasabbas18@gmail.com>
2025-06-11 01:43:19 -0300

    ♻️ refactor: replace inline handlers with separate handler functions

--- router/routers.go
+++ router/routers.go
@@ -1,38 +1,21 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/nicolas-nva/opportunities-go/handlers"
 )
 
 func initializeRoutes(router *gin.Engine){
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET opening",
-			})
-		})
-		v1.POST("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "POST opening",
-			})
-		})
-		v1.DELETE("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "DELETE opening",
-			})
-		})
-		v1.PUT("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "PUT opening",
-			})
-		})
-		v1.GET("/openings", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET openings",
-			})
-		})     
+		v1.GET("/opening", handlers.ListOpeningHandler)
+
+		v1.POST("/opening", handlers.CreateOpeningHandler)
+
+		v1.DELETE("/opening", handlers.DeleteOpeningHandler)
+
+		v1.PUT("/opening", handlers.UpdateOpeningHandler)
+		
+		v1.GET("/openings", handlers.ListOpeningsHandler)     
 	}
 }
